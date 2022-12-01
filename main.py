22cd278f85bca75a9671ead27f0ce6eb2ee0903f
sadam <sadamccr@gmail.com>
2022-12-02 02:34:20 +0500

    configuration and threading done

--- main.py
+++ main.py
@@ -23,7 +23,64 @@
 #         if book.tag == 'book':
 #             books.append(book_dict)
 #             book_dict = {}
-#     return books
 #
 #
-# my_books = parse_book_xml('books.xml')
+# parse_book_xml('books.xml')
+#
+
+# #-------- Configuration -------------
+
+# from configparser import ConfigParser
+#
+# config = ConfigParser()
+#
+# config['DEFAULT'] = {
+#     'title': 'Hello World',
+#     'compression': 'yes',
+#     'compression_level': 9,
+# }
+# config['database'] = {}
+# database = config['database']
+# database['host'] = '127.0.0.1'
+# database['user'] = 'username'
+# database['pass'] = 'password'
+# database['keep-alive'] = 'no'
+#
+# with open('config.ini', 'w') as configfile:
+#     config.write(configfile)
+#
+
+# #-------- Threading -------------
+#
+# import datetime
+# import threading
+#
+#
+# def get_cubic(num):
+#     print(datetime.datetime.now())
+#     print(num * num * num)
+#
+#
+# def get_inverse(num):
+#     print(datetime.datetime.now())
+#     print(1 / num)
+#
+#
+# get_cubic(10)
+# get_inverse(10)
+#
+#
+# t1 = threading.Thread(target=get_cubic, args=(10,))
+# t2 = threading.Thread(target=get_inverse, args=(10,))
+#
+# t2.start()
+# t1.start()
+#
+# t2.join()
+# t1.join()
+#
+# print('Done!')
+
+
+
+
